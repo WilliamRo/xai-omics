6ede2dd1ae20af98bf24095d3a9c7fa2b41643a7
WilliamRo <walienluo@gmail.com>
2023-08-31 14:59:18 +0800

    [M] added function demo in uld explorer

--- dev/explorers/uld_explorer/uld_explorer.py
+++ dev/explorers/uld_explorer/uld_explorer.py
@@ -1,10 +1,10 @@
-import matplotlib.pyplot as plt
-
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 from xomics.gui.dr_gordon import DrGordon, SliceView, Plotter
 from xomics import MedicalImage
 
-import os
+import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 
 
@@ -62,6 +62,60 @@ class HistogramViewer(Plotter):
 
 
 
+class DeltaViewer(SliceView):
+
+  def __init__(self, dr_gordon=None):
+    # Call parent's constructor
+    super().__init__(pictor=dr_gordon)
+
+    self.new_settable_attr('cmap', 'RdBu', str, 'Color map')
+    self.new_settable_attr(
+      'alpha', 1.0, float,
+      'Coefficient before low dose image while calculating delta')
+
+    self.settable_attributes.pop('vmin')
+
+
+  def view_slice(self, fig: plt.Figure, ax: plt.Axes, x: int):
+    mi: MedicalImage = self.selected_medical_image
+
+    # Show slice
+    full_dose_slice = mi.images['Full'][x]
+    image: np.ndarray = mi.images[self.displayed_layer_key][x]
+    delta = full_dose_slice - self.get('alpha') * image
+
+    abs_vmax = (np.max(abs(delta)) if self.get('vmax') is None
+                else self.get('vmax'))
+
+    im = ax.imshow(delta, cmap=self.get('cmap'), vmin=-abs_vmax,
+                   vmax=abs_vmax)
+
+    # Set style
+    ax.set_axis_off()
+
+    # Show color bar if required
+    if self.get('color_bar'):
+      divider = make_axes_locatable(ax)
+      cax = divider.append_axes('right', size='5%', pad=0.05)
+      fig.colorbar(im, cax=cax)
+
+
+  def optimize_alpha(self, stategy='alpha',
+                     alphas: str = '0.9,0.95,1.0,1.05,1.1'):
+    """Calculate losses given alphas"""
+    assert stategy == 'alpha'
+    mi: MedicalImage = self.selected_medical_image
+
+    full = mi.images['Full']
+    image: np.ndarray = mi.images[self.displayed_layer_key]
+
+    alphas = [float(a) for a in alphas.split(',')]
+    losses = [abs(np.mean(full - a * image)) for a in alphas]
+
+    for l, a in zip(losses, alphas): print(f'.. loss({a}) = {l}')
+
+
+
 if __name__ == '__main__':
   data_dir = r'../../../data/01-ULD/'
 
@@ -81,4 +135,9 @@ if __name__ == '__main__':
   ue.add_plotter(HistogramViewer())
   ue.dv.set('vmin', auto_refresh=False)
   ue.dv.set('vmax', auto_refresh=False)
+
+  delta_viewer = DeltaViewer()
+  delta_viewer.set('vmax', auto_refresh=False)
+  ue.add_plotter(delta_viewer)
+
   ue.show()
