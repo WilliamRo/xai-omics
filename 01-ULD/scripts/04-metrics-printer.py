bb2532a89aab14862add2584b61275e865feddda
Jimmy <2489219080@qq.com>
2023-09-20 17:00:40 +0800

    [m] fix uld 04 scripts

--- 01-ULD/scripts/04-metrics-printer.py
+++ 01-ULD/scripts/04-metrics-printer.py
@@ -1,21 +1,23 @@
+import numpy as np
+
 from utils.metrics_calc import get_metrics
-from xomics.data_io.npy_reader import load_data
+from xomics.data_io.uld_reader import UldReader
 
 dose_tags = [
-  'Full',
-  '1-2',
-  '1-4',
-  '1-10',
-  '1-20',
-  '1-50',
-  '1-100',
+  ['Full'],
+  ['1-2'],
+  ['1-4'],
+  ['1-10'],
+  ['1-20'],
+  ['1-50'],
+  ['1-100'],
 ]
 
 
-def output_metrics(arr1, arr2, metrics: list):
-  result = get_metrics(arr1, arr2, metrics)
+def output_metrics(arr1, arr2, metrics: list, **kwargs):
+  result = get_metrics(arr1, arr2, metrics, **kwargs)
   for metric, value in result.items():
-    print(f'{metric}: {value}')
+    print(f'{metric}: {value: .5f}')
 
   return result
 
@@ -23,12 +25,16 @@ def output_metrics(arr1, arr2, metrics: list):
 if __name__ == '__main__':
   dirpath = '../../data/01-ULD/'
   shape = [1, 608, 440, 440, 1]
-  full = load_data(dirpath, 6, dose_tags[0], shape=shape)[0, ..., 0]
-  print(full.shape)
+  subjects = [2]
 
-  metr = ['nrmse', 'SSIM', 'psnr', 'pw_rmse']
-  for i in range(len(dose_tags[1:])):
-    print(f'{dose_tags[1:][i]} dose:')
-    img = load_data(dirpath, 6, dose_tags[1:][i], shape=shape)[0, ..., 0]
-    output_metrics(full, img, metr)
+  reader = UldReader(dirpath)
+  imgs = reader.load_data(subjects, dose_tags, methods='type', shape=shape, raw=True)
+  full = imgs['Full'][0][0, ..., 0]
+
+  metr = ['nrmse', 'SSIM', 'psnr'] # , 'pw_rmse']
+  for k, v in imgs.items():
+    if k == 'Full': continue
+    print(f'{k} dose:')
+    norm = np.max(v[0][0, ..., 0])
+    output_metrics(full/norm, v[0][0, ..., 0]/norm, metr, data_range=1.0)
 
