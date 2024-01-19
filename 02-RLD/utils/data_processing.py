8b692f7dc5de0f86ca336465173a11d68c099643
Jimmy <2489219080@qq.com>
2024-01-19 17:47:51 +0800

    [M] speedup the preprocess

--- 02-RLD/utils/data_processing.py
+++ 02-RLD/utils/data_processing.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from skimage import restoration
 
 
 # self.features/targets.shape = [N, S, H, W, 2]
@@ -37,12 +37,14 @@ def get_random_window(arr: np.ndarray, window_size=128, slice_size=16,
 
   arr = arr != 0
   arr = arr[index, :, :, :, 0]
-  s_arr = np.any(arr == 1, axis=(1, 2))
-  h_arr = np.any(arr == 1, axis=(0, 2))
-  w_arr = np.any(arr == 1, axis=(0, 1))
-  distr_s = normalize(s_arr.reshape((-1)))
-  distr_w = normalize(h_arr.reshape((-1)))
-  distr_h = normalize(w_arr.reshape((-1)))
+
+  s_arr = np.any(arr, axis=(1, 2))
+  h_arr = np.any(arr, axis=(0, 2))
+  w_arr = np.any(arr, axis=(0, 1))
+
+  distr_s = normalize(s_arr.ravel())
+  distr_w = normalize(h_arr.ravel())
+  distr_h = normalize(w_arr.ravel())
 
   s = windows_choose(distr_s, slice_size)
   h = windows_choose(distr_h, window_size)
@@ -72,38 +74,56 @@ def gen_windows(arr1: np.ndarray, arr2: np.ndarray, batch_size,
   return features, targets
 
 
+def nonlocal_mean(image, h=10, patch_size=7, patch_dis=21):
+  de_img = restoration.denoise_nl_means(image, h=h,
+                                        patch_size=patch_size,
+                                        patch_distance=patch_dis)
+
+  return de_img
+
+
+
 
 
 if __name__ == '__main__':
   from xomics import MedicalImage
   from xomics.objects.general_mi import GeneralMI
-  from xomics.gui.dr_gordon import DrGordon
-  import SimpleITK as sitk
+  from dev.explorers.rld_explore.rld_explorer import RLDExplorer
+  import datetime
 
-  a = GeneralMI.load_img('../06_puadap/raw_data/YHP00012417-low.nii.gz')
-  b = GeneralMI.load_img('../06_puadap/raw_data/YHP00012417-full.nii.gz')
+  test_gen_win = True
 
-  a = sitk.GetArrayFromImage(a)
-  a = np.expand_dims(a, axis=[-1, 0])
-  b = sitk.GetArrayFromImage(b)
-  b = np.expand_dims(b, axis=[-1, 0])
+  test = GeneralMI.get_test_sample(r'../../data/02-RLD/rld_data.csv')
+  test.process_param['shape'] = [440, 440, 480]
+  test.process_param['norm'] = 'PET'
 
-  num = 4
-  img_f, img_t = gen_windows(a, b, num, true_rand=False)
-  # print(len(img))
-  # print(a.shape, img[0].shape)
-  di_f = {}
-  di_t = {}
-  for i in range(num):
-    di_f[f'feature-{i}'] = img_f[i]
-  for i in range(num):
-    di_t[f'target-{i}'] = img_t[i]
-  mi_f = MedicalImage('feature', di_f)
-  mi_t = MedicalImage('target', di_t)
-  dg = DrGordon([mi_f, mi_t])
-  dg.slice_view.set('vmin', auto_refresh=False)
-  dg.slice_view.set('vmax', auto_refresh=False)
+  a = test.images['30G'][0]
+  b = test.labels['240G'][0]
+  if test_gen_win:
+    a = np.expand_dims(a, axis=[-1, 0])
+    b = np.expand_dims(b, axis=[-1, 0])
+
+    num = 16
+    time1 = datetime.datetime.now()
+    img_f, img_t = gen_windows(a, b, num, true_rand=False)
+    time2 = datetime.datetime.now()
+    print("time:", time2 - time1)
+    di_f = {}
+    di_t = {}
+    for i in range(num):
+      di_f[f'feature-{i}'] = img_f[i]
+    for i in range(num):
+      di_t[f'target-{i}'] = img_t[i]
+    mi_f = MedicalImage('feature', di_f)
+    mi_t = MedicalImage('target', di_t)
+    mis = [mi_f, mi_t]
+  else:
+    da = nonlocal_mean(a, h=1000, patch_size=3, patch_dis=3)
+    print(da.shape)
+    mis = [MedicalImage('feature', {'nonlocal': da, 'raw': a, 'full': b})]
+    pass
+
+  dg = RLDExplorer(mis)
+  dg.sv.set('vmin', auto_refresh=False)
+  dg.sv.set('vmax', auto_refresh=False)
   dg.show()
-  # print(get_center(arr, 128).shape)
-  # prob = calc_prob(arr, 128)
-  # print(prob.shape)
