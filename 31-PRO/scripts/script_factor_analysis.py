87acd407c47ff26e2279a8986c56623ece41fcad
GordonWangch <gordonwangch@gmail.com>
2024-04-01 19:28:41 +0800

    Gordon modified 31-PRO

---
+++ 31-PRO/scripts/script_factor_analysis.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+
+from scipy.stats import mannwhitneyu, ttest_ind
+
+
+
+if __name__ == "__main__":
+  data = pd.read_csv("f_new305.csv")
+
+  label = data['lab']
+  label0_index = np.where(label == 0)[0]
+  label1_index = np.where(label == 1)[0]
+
+  df_features = data[data.columns[1:]]
+  feature_names = df_features.columns
+
+  threshold_mul_p = 0.1
+
+  result_dict = {}
+  multivariable_features = []
+  for f in feature_names:
+    feature = df_features[f]
+    feature_label0 = feature[label0_index]
+    feature_label1 = feature[label1_index]
+
+    if 'tPSA' == f:
+      statistic, p_value = mannwhitneyu(feature_label0, feature_label1)
+      result_dict[f] = ['median', p_value]
+    else:
+      statistic, p_value = ttest_ind(
+        feature_label0, feature_label1, equal_var=False)
+      result_dict[f] = ['mean', p_value]
+
+    if p_value < threshold_mul_p: multivariable_features.append(f)
+
+  # Multifactor analysis
+  data['intercept'] = 1
+  model = sm.GLM(
+    data['lab'], data[multivariable_features + ['intercept']],
+    family=sm.families.Gaussian()).fit()
+
+  for d in model.summary().tables[1].data:
+    feature, p_value = d[0], d[4]
+    if feature not in result_dict.keys(): continue
+    else:
+      result_dict[feature] = result_dict[feature] + [float(p_value)]
+
+  for f, d in result_dict.items():
+    if len(d) == 2:
+      method = d[0]
+      p_uni = '<0.001' if d[1] < 0.001 else f'{d[1]: .3f}'
+      print(f'{f: <15}{method: <15}{p_uni: <15}')
+    else:
+      method = d[0]
+      p_uni = '<0.001' if d[1] < 0.001 else f'{d[1]: .3f}'
+      p_mul = '<0.001' if d[2] < 0.001 else f'{d[2]: .3f}'
+      print(f'{f: <15}{method: <15}{p_uni: <15}{p_mul: <15}')
